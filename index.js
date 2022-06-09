0b0a2b6f66f082ed7d6cd30e0c4c0a1abd5e058a
sooryasrajan <soorya.s27@gmail.com>
2022-06-10 01:32:48 +0530

    Added all the necessary files and directories, added basic content to index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 8000
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.send(`Server running on port ${PORT}`)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
